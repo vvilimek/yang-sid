627510f88fdf8f2d5741fe40368c3a16953651b1
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-05-12 17:24:53 +0200

    typing: Move .sid file types from stub file

--- src/yang_sid/_sid_file_types.py
+++ src/yang_sid/_sid_file_types.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from typing import TypedDict, Optional, Any
+from typing import TypedDict, Optional, Any, Literal, overload
 
 import yangson.instproto
 
@@ -28,8 +28,25 @@ class ItemDict(TypedDict, total=False):
     sid: yangson.instproto.IntInstanceNode
 
 class SidFileDict(yangson.instproto.InstanceNodeProtocol):
+    @overload
+    def __getitem__(self, key: Literal["module-name"]) -> yangson.instproto.StrInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["module-revision"]) -> yangson.instproto.StrInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["sid-file-version"]) -> yangson.instproto.IntInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["sid-file-status"]) -> yangson.instproto.StrInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["dependency-revision"]) -> yangson.instproto.ArrayInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["assignment-range"]) -> yangson.instproto.ArrayInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["item"]) -> yangson.instproto.ArrayInstanceNode: ...
+    @overload
+    def __getitem__(self, key: Literal["description"]) -> yangson.instproto.StrInstanceNode: ...
+
     def __getitem__(self, key: Literal["module-name", "module-revision", "sid-file-version", "sid-file-status", "dependency-revision", "assignment-range", "item", "description"]) -> yangson.instproto.StrInstanceNode | yangson.instproto.IntInstanceNode | yangson.instproto.ArrayInstanceNode:
-        pass
+        return super().__getitem__(key)
 
 class SidFileBuilder(TypedDict, total=False):
     module: str
